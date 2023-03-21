d6be13954bc7b7450973b8bec2336d777ac2b9cf
zezerinodev <mattoszz@hotmail.com>
2023-03-21 11:17:35 -0300

    Atualização, da pagina guia gratuito

--- guia-cortesia/script.js
+++ guia-cortesia/script.js
@@ -1,49 +1,3 @@
-// function validarForm() {
-//     // Obter o valor do input
-//     let nome = $('form input#nome').val();
-//     let email = $('form input#email').val();
-//     let telefone = $('form input#telefone').val();
-  
-//     // Verificar se o valor do input está vazio
-//     if (nome === '' || email === '' || telefone === '') {
-//       // Exibir o alerta
-//       alert('Por favor, Para ter o nosso Guia Gratuito você precisa informar seus dados corretamente.');
-//       return false;
-//     }else{
-
-        
-//         /*--- Enviando email pelo FORMSPRE -------*/
-//         $.ajax({
-//             url: "https://formspree.io/f/mnqyznjq", 
-//             method: "POST",
-//             data: {
-//                 nome: $("#nome").val(),
-//                 email: $("#email").val(),
-//                 telefone: $("#telefone").val(), 
-//             },
-//             dataType: "json"
-//             }).done(function(){
-//                 $("#nome").val("");
-//                 $("#email").val("");
-//                 $("#telefone").val("");
-//                 alert("Email enviado com sucesso!");
-//             }).fail(function(){
-//                 alert("Erro ao enviar email!");
-//         });
-
-//         $('div#guiaGratuito').css('display', 'flex');
-
-//         $('form input#nome').val('');
-//         $('form input#email').val('');
-//         $('form input#telefone').val('');
-
-        
-//     }
-
-
-// }
-
-
 
 /*--------------------------*/
 /*  ELEMENTOS DO CONTATO    */
@@ -69,7 +23,6 @@ $(function($){
 			$("#email").val("");
             $("#telefone").val("");
             $('div#guiaGratuito').css('display', 'flex');
-			alert("Email enviado com sucesso!");
 		}).fail(function(){
 			alert("Erro ao enviar email!");
 		});
